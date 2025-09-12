71a748005e378c43b1962e25e1a8b40eb590d8cf
Harshita Sinha <204292568+Harshu52-ai@users.noreply.github.com>
2025-09-12 09:27:17 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,8 +1,3 @@
 #pragma once
 
-
-bool isTemperatureInRange(float temperature);
-bool isPulseRateInRange(float pulseRate);
-bool isSpoInRange(float spo2);
-bool vitalsInRange(float temperature, float pulseRate, float spo2);
-void alert(const char* message);
+int vitalsOk(float temperature, float pulseRate, float spo2);
